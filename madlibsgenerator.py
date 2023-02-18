88bf8faf4b0cbe5342742706f58290f5c7f2bd2f
Vhivi <david.angelelli@hotmail.fr>
2023-02-18 14:58:50 +0100

    Ajout du fichier madlibsgenerator.py

---
+++ madlibsgenerator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Mad Libs Generator
+# La conception du programme est telle qu'il demandera aux utilisateurs
+# d'entrer une série d'entrées qui seront considérées comme un Mad Lib.
+#
+# L'entrée peut être n'importe quoi, un adjectif, un nom, un pronom, etc. Une
+# fois toutes les entrées saisies, l'application prendra les données et les
+# organisera sous la forme d'un modèle d'histoire. Puis affichera le résultat.
+
+
